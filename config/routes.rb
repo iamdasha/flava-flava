b08876eb0c2c9338569068dab4f907d4b9fe4e9e
iamdasha <daria.g2424@gmail.com>
2021-10-22 21:04:08 +0300

    about html

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
+  get 'about/index'
   resources :posts
   resources :subscribers
   get 'promo/index'
 
   # root 'promo#index'
   get 'posts/index'
+  get 'about/index'
+
 
   get 'promo', to: 'promo#index'
-  # get 'about', to: 'about#index'
+  get 'about', to: 'about#index'
 
   # root 'promo#index'
   root 'posts#index'
