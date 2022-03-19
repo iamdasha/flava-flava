49c1a690b7d4a1d17a77e34b50b82a5506913577
iamdasha <daria.g2424@gmail.com>
2022-03-19 18:37:02 +0300

    component

---
+++ app/javascript/components/A_Text.js
@@ -0,0 +1,14 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+class A_Text extends React.Component {
+  render () {
+    return (
+      <h1>
+        Flava
+      </h1>
+    );
+  }
+}
+
+export default A_Text
